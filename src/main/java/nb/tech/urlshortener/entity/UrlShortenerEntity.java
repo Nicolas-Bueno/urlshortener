cfc60067c8f2cc410daa56f4725726c8a814c1ab
Nicolas Bueno <nicolasbrian202@gmail.com>
2024-05-05 11:47:22 -0300

    create entity and repository

---
+++ src/main/java/nb/tech/urlshortener/entity/UrlShortenerEntity.java
@@ -0,0 +1,36 @@
+package nb.tech.urlshortener.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Indexed;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table (name = "tb_urlShortener")
+public class UrlShortenerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String urlOrigin;
+
+    private String urlShorterner;
+
+    private LocalDateTime expirationTime;
+
+}
---
+++ src/main/java/nb/tech/urlshortener/repository/UrlShortenerRepository.java
@@ -0,0 +1,14 @@
+package nb.tech.urlshortener.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import nb.tech.urlshortener.entity.UrlShortenerEntity;
+
+public interface UrlShortenerRepository extends JpaRepository<UrlShortenerEntity, Long>{
+
+    List<UrlShortenerEntity> findByExpirationTimeBefore(LocalDateTime expirationTime);
+
+}
